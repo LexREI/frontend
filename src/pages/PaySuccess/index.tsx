9d3f449e8fd5803465b053b0d7b82eb14c08d4d2
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2023-11-12 14:04:13 -0800

    remove: unused code

--- src/pages/PaySuccess/index.tsx
+++ src/pages/PaySuccess/index.tsx
@@ -1,56 +1,49 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import HomeHeader from '@/layouts/Header/HomeHeader';
-import { useContext, useEffect, useState } from 'react';
-import {
-  getUserSubscriptionSession,
-  paymentSuccessUsingPost,
-} from '@/services/SubscriptionController';
-import { Button } from '@/components/ui/button';
-import { AuthContext } from '@/contexts/auth_context';
 
 export default function PaySuccess() {
-  const { getCurrentUser } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const [userSubscriptionSession, setUserSubscriptionSession] =
-    useState<API.UserSubscriptionSessionResponse>();
-
-  const getSubscriptionSession = async () => {
-    try {
-      const res = await getUserSubscriptionSession();
-      if (res.data.code === 200) {
-        setUserSubscriptionSession(res.data.data);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const handlePaymentSuccess = async () => {
-    try {
-      const body: API.CheckoutSessionBody = {
-        _id: userSubscriptionSession?._id || '',
-        session_id: userSubscriptionSession?.session_id || '',
-      };
-      const res = await paymentSuccessUsingPost(body);
-      if (res.data.code === 200) {
-        await getCurrentUser();
-        navigate('/pricing');
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    getSubscriptionSession();
-  }, []);
+  // const { getCurrentUser } = useContext(AuthContext);
+  // const navigate = useNavigate();
+  // const [userSubscriptionSession, setUserSubscriptionSession] =
+  //   useState<API.UserSubscriptionSessionResponse>();
+  //
+  // const getSubscriptionSession = async () => {
+  //   try {
+  //     const res = await getUserSubscriptionSession();
+  //     if (res.data.code === 200) {
+  //       setUserSubscriptionSession(res.data.data);
+  //     }
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+  //
+  // const handlePaymentSuccess = async () => {
+  //   try {
+  //     const body: API.CheckoutSessionBody = {
+  //       _id: userSubscriptionSession?._id || '',
+  //       session_id: userSubscriptionSession?.session_id || '',
+  //     };
+  //     const res = await paymentSuccessUsingPost(body);
+  //     if (res.data.code === 200) {
+  //       await getCurrentUser();
+  //       navigate('/pricing');
+  //     }
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+  //
+  // useEffect(() => {
+  //   getSubscriptionSession();
+  // }, []);
   return (
     <div className="h-screen">
       <HomeHeader />
       <main className="h-full home_background isolate">
         <div className="py-40">
           <p>pay success</p>
-          <Button onClick={() => handlePaymentSuccess()}>Proceed</Button>
+          <Link to="/pricing">Proceed</Link>
         </div>
       </main>
     </div>
